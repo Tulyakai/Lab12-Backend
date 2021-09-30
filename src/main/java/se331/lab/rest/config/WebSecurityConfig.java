b369e2164d2e6bdf0f82dbdae4d659f019abb704
Tulyakai <tulyawatt@gmail.com>
2021-09-30 18:12:35 +0700

    6.8

--- src/main/java/se331/lab/rest/config/WebSecurityConfig.java
+++ src/main/java/se331/lab/rest/config/WebSecurityConfig.java
@@ -68,6 +68,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/events").permitAll()
                 .antMatchers(HttpMethod.GET, "/organizers").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/events").hasRole("ADMIN")
+
 
                 .anyRequest().authenticated();
 
